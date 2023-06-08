042de2285a9cb725b8b602f59761d49caff4e4a6
a17876 <a17876@hanmail.net>
2023-06-08 12:26:32 -0700

    code action and extract MyButton

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
       backgroundColor: const Color(0xFF181818),
       body: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 40,
+          horizontal: 20,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -72,10 +72,12 @@ class App extends StatelessWidget {
               height: 30,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                const MyButton(),
                 Container(
                   decoration: BoxDecoration(
-                    color: const Color(0xFFF2B33A),
+                    color: const Color(0xFF1F2123),
                     borderRadius: BorderRadius.circular(45),
                   ),
                   child: const Padding(
@@ -84,10 +86,8 @@ class App extends StatelessWidget {
                       horizontal: 50,
                     ),
                     child: Text(
-                      'Transfer',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
+                      'Request',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
                   ),
                 ),
@@ -97,5 +97,33 @@ class App extends StatelessWidget {
         ),
       ),
     ));
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFF2B33A),
+        borderRadius: BorderRadius.circular(45),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 50,
+        ),
+        child: Text(
+          'Transfer',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
   }
 }
